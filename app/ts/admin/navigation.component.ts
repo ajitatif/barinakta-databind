f52e023238a4f5d06d0f30c556492e93165937eb
ajitatif <gokalp.gurbuzer@yandex.com>
2017-04-03 22:48:13 +0300

    - upgraded angular version and dependencies - fixed compile errors

--- app/ts/admin/navigation.component.ts
+++ app/ts/admin/navigation.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'admin-nav',
-    moduleId: module.id;
+    moduleId: module.id,
     templateUrl: '../../ui/admin/navigation.html'
 })
 export class AdminNavigationComponent {}
